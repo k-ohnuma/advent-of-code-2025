de7dc060ea44133d8b8862aaea6cfaa59296d779
ohnuma <k.a.y.0818.k@gmail.com>
2025-12-02 15:06:33 +0900

    day2

---
+++ src/bin/2-1.rs
@@ -0,0 +1,45 @@
+use std::cmp::max;
+
+use itertools::Itertools;
+use proconio::input;
+
+fn parse_input(s: &str) -> Vec<(usize, usize)> {
+    let hi = s.split(',').collect_vec();
+    hi.into_iter()
+        .map(|e| {
+            let sp = e
+                .split('-')
+                .map(|e| e.parse::<usize>().unwrap())
+                .collect_vec();
+            (sp[0], sp[1])
+        })
+        .collect_vec()
+}
+
+fn main() {
+    input! {
+        s: String
+    }
+    let ab = parse_input(s.as_str());
+    let ma = ab
+        .iter()
+        .map(|&e| max(e.0, e.1))
+        .max()
+        .unwrap()
+        .to_string()
+        .len();
+    let mut ans = 0;
+    for num in 1usize.. {
+        let rep = format!("{}{}", num, num).parse::<usize>().unwrap();
+        if rep.to_string().len() > ma {
+            break;
+        }
+        for &(l, r) in ab.iter() {
+            if l <= rep && rep <= r {
+                ans += rep;
+                break;
+            }
+        }
+    }
+    println!("{}", ans);
+}
---
+++ src/bin/2-2.rs
@@ -0,0 +1,52 @@
+use std::{cmp::max, collections::HashSet};
+
+use itertools::Itertools;
+use proconio::input;
+
+fn parse_input(s: &str) -> Vec<(usize, usize)> {
+    let hi = s.split(',').collect_vec();
+    hi.into_iter()
+        .map(|e| {
+            let sp = e
+                .split('-')
+                .map(|e| e.parse::<usize>().unwrap())
+                .collect_vec();
+            (sp[0], sp[1])
+        })
+        .collect_vec()
+}
+
+fn main() {
+    input! {
+        s: String
+    }
+    let ab = parse_input(s.as_str());
+    let ma = ab
+        .iter()
+        .map(|&e| max(e.0, e.1))
+        .max()
+        .unwrap()
+        .to_string()
+        .len();
+    let mut ans = HashSet::new();
+    for num in 1usize.. {
+        let rep = num.to_string().repeat(2).parse::<usize>().unwrap();
+        if rep.to_string().len() > ma {
+            break;
+        }
+        for rc in 2.. {
+            let rep = num.to_string().repeat(rc).parse::<usize>().unwrap();
+            if rep.to_string().len() > ma {
+                break;
+            }
+            for &(l, r) in ab.iter() {
+                if l <= rep && rep <= r {
+                    ans.insert(rep);
+                    break;
+                }
+            }
+        }
+    }
+    let ans = ans.iter().sum::<usize>();
+    println!("{}", ans);
+}
